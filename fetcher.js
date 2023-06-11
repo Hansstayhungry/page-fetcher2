3dadd0b44cd5960f30bb81a0169dc4a39cbdf2c2
Hans Wang <hansmikaa@live.cn>
2023-06-11 19:43:31 -0400

    finish page downloader

---
+++ fetcher.js
@@ -0,0 +1,24 @@
+const request = require('request');
+const fs = require('fs');
+
+const args = process.argv;
+const url = args[2];
+const path = args[3];
+
+const downloader = function(url, path) {
+  request(url, (error, response, body) => {
+    if (error) {
+      console.log('error: ', error);
+    } else {
+      fs.writeFile(path, body, err => {
+        if (err) {
+          console.error('Error writing files: ', err);
+        } else {
+          console.log('File download successfully.');
+        }
+      })
+    }
+  })
+};
+
+downloader(url, path);
